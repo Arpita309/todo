67fe4b1406180890a9cfdc6003c9a10a43e103c5
arpita tiwari <arpitatiwari309@gmail.com>
2020-03-18 18:50:38 +0530

    build

---
+++ build/precache-manifest.467d5957cc5f2a5e07e8b518d7de1601.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70585d62f56fdf14f1a7d0cff880c221",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd03252988a54cc1fba5",
+    "url": "/static/css/2.5dabca3a.chunk.css"
+  },
+  {
+    "revision": "cd03252988a54cc1fba5",
+    "url": "/static/js/2.43744638.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.43744638.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2b4d979849598dc1e42",
+    "url": "/static/js/main.54adedcd.chunk.js"
+  },
+  {
+    "revision": "452efe56c2036ff11208",
+    "url": "/static/js/runtime-main.83c66be5.js"
+  }
+]);
